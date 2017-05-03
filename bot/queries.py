c3d577bcff2465b812a3be441b2d86d57698203d
andrewgolman <gas1998@mail.ru>
2017-05-03 21:30:54 +0300

    Registration debugged

--- bot/queries.py
+++ bot/queries.py
@@ -3,27 +3,48 @@ import psycopg2
 base = psycopg2.connect("dbname='cardkeepersample' user='andrew' host='localhost' password='1234'")
 cursor = base.cursor()
 
+# Wrapped queries in alphabetic order
+
+def push():
+    cursor.execute("COMMIT;")
+
+
+def add_user(user):
+    query = """INSERT INTO users (user_id, name, general_goal, weekly_goal, notifications_learn, notifications_stats, joined)
+                VALUES ('{}', '{}', '{}', '{}', '{}', '{}', current_date);""".format(*user)
+    cursor.execute(query)
+    push()
 
 def if_registered(user):
-    query = "SELECT * FROM users WHERE users.user_id = '{}'".format(user)
+    query = "SELECT * FROM users WHERE users.user_id = '{}';".format(user)
     cursor.execute(query)
     return True if len(cursor.fetchall()) else False
 
 
 def if_username_taken(username):
-    query = "SELECT * FROM users WHERE users.name = '{}'".format(username)
+    query = "SELECT * FROM users WHERE users.name = '{}';".format(username)
     cursor.execute(query)
     return True if len(cursor.fetchall()) else False
 
 
-def new_pack(name, owner, privacy='public'):
-    query = "INSERT INTO packs (name, owner_id, privacy) VALUES ('{}' '{}' '{}')".format(name, owner, privacy)
-    cursor.execute(query)
-
-
 def new_card(front, back):
-    query = "INSERT INTO cards (front, back) VALUES ('{}', '{}')".format(front, back)
+    query = "INSERT INTO cards (front, back) VALUES ('{}', '{}');".format(front, back)
+    push()
     cursor.execute(query)
+
+
+def new_pack(name, owner, privacy='public', status='active'):
+    query = "INSERT INTO packs (name, owner_id, privacy) VALUES ('{}' '{}' '{}');".format(name, owner, privacy)
+    cursor.execute(query)
+    push()
+    query = "SELECT pack_id FROM packs WHERE name = {} AND owner_id = {};".format(name, owner)
+    cursor.execute(query)
+    pack_id = cursor.fetchall()[0].pack_id
+    query = "INSERT INTO user_packs (user_id, pack_id, status) VALUES ('{}', '{}', '{}'));".format(owner, pack_id, status)
+    cursor.execute(query)
+    push()
+    return pack_id
+
 
 def display_active_packs(user_id):
-    pass
+    query = ""
--- bot/register.py
+++ bot/register.py
@@ -5,10 +5,13 @@ import say
 
 GENERAL_GOAL, WEEKLY_GOAL, NOTIFY_LEARN, NOTIFY_STATS = (0, 1, 2, 3)
 
+users_in_process = {}
+
 
 def start(bot, update):
     user_id = update.message.from_user.id
     name = update.message.from_user.username
+    users_in_process[user_id] = [user_id, name]
 
     if queries.if_registered(user_id):
         update.message.reply_text(say.hello)
@@ -17,20 +20,22 @@ def start(bot, update):
 
     update.message.reply_text(say.welcome)
 
-    reply_keyboard = [['Live speaking', 'Belletristic', 'Science']]
+    reply_keyboard = [['Speech', 'Belletristic', 'Science']]
     update.message.reply_text(say.choose_general_goal,
                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return GENERAL_GOAL
 
 
 def general_goal(bot, update):
-    general_goal = update.message.reply_text
+    general_goal = update.message.text
+    users_in_process[update.message.from_user.id].append(general_goal)
     update.message.reply_text(say.choose_weekly_goal)
     return WEEKLY_GOAL
 
 
 def weekly_goal(bot, update):
-    weekly_goal = update.message.reply_text
+    weekly_goal = update.message.text
+    users_in_process[update.message.from_user.id].append(weekly_goal)
     reply_keyboard = [['Daily', 'Weekly', 'Never']]
     update.message.reply_text(say.choose_learn_notifications,
                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
@@ -38,7 +43,8 @@ def weekly_goal(bot, update):
 
 
 def notify_learn(bot, update):
-    notify_learn = update.message.reply_text
+    notify_learn = update.message.text
+    users_in_process[update.message.from_user.id].append(notify_learn)
     reply_keyboard = [['Daily', 'Weekly', 'Never']]
     update.message.reply_text(say.choose_stats_notifications,
                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
@@ -46,9 +52,12 @@ def notify_learn(bot, update):
 
 
 def notify_stats(bot, update):
-    notify_stats = update.message.reply_text
+    notify_stats = update.message.text
+    users_in_process[update.message.from_user.id].append(notify_stats)
     update.message.reply_text(say.registration_completed)
     menu(bot, update)
+    print(users_in_process[update.message.from_user.id])
+    queries.add_user(users_in_process.pop(update.message.from_user.id))
     return ConversationHandler.END
 
 
