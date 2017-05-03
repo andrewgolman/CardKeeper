7613db1af24dde310a21e6e55eb9acf2d7ccf2f7
andrewgolman <gas1998@mail.ru>
2017-05-03 19:39:08 +0300

    Replace user id with telegram  id

--- create.sql
+++ create.sql
@@ -6,24 +6,25 @@ CREATE TYPE RIGHTS_TYPE AS ENUM ('admin', 'user') ;
 CREATE TYPE INVITATION_TYPE AS ENUM ('from a group', 'from a user') ; 
 
 CREATE TABLE users (
-    user_id 			SERIAL 		PRIMARY KEY,
-    name 				VARCHAR(30) 	NOT NULL UNIQUE,
-    joined 			DATE			NOT NULL,
-    general_goal 		GEN_GOAL_TYPE,
-    weekly_goal 		INTEGER 		NOT NULL,
+    user_id 			        INTEGER 		PRIMARY KEY,
+    name 				          VARCHAR(30) 	NOT NULL UNIQUE,
+    joined 			          DATE			NOT NULL,
+    general_goal          GEN_GOAL_TYPE,
+    weekly_goal 	        INTEGER 		NOT NULL,
     notifications_learn 	NOTIFICATION_TYPE,
     notifications_stats 	NOTIFICATION_TYPE
 );
 
 CREATE TABLE groups (
-    group_id 	SERIAL 		PRIMARY KEY,
-    name 		VARCHAR(50) 	NOT NULL UNIQUE,
-    privacy 	PRIVACY_TYPE 	NOT NULL,
-    founder 	INTEGER 		REFERENCES users (user_id)
+    group_id 	  SERIAL 		PRIMARY KEY,
+    name 		    VARCHAR(50) 	NOT NULL UNIQUE,
+    privacy 	  PRIVACY_TYPE 	NOT NULL,
+    founder 	  INTEGER 		REFERENCES users (user_id)
 );
 
 CREATE TABLE packs (
     pack_id 	SERIAL 	PRIMARY KEY,
+    name      VARCHAR(30) NOT NULL UNIQUE,
     owner_id 	INTEGER 	REFERENCES users (user_id),
     privacy 	PRIVACY_TYPE
 );
@@ -32,57 +33,51 @@ CREATE TABLE cards (
     card_id 	SERIAL 	PRIMARY KEY,
     pack_id 	INTEGER 	REFERENCES packs (pack_id),
     front 		TEXT 		NOT NULL,
-    back 		TEXT,
+    back 		  TEXT,
     comment 	TEXT
 );
 
 CREATE TABLE user_groups (
     group_id 	INTEGER 	REFERENCES groups (group_id),
-    user_id 	INTEGER 	REFERENCES users (user_id)
-
+    user_id 	INTEGER 	REFERENCES users (user_id),
     PRIMARY KEY (group_id, user_id)
 );
 
 CREATE TABLE user_packs (
-    pack_id 		INTEGER 			REFERENCES packs (pack_id),
-    user_id 		INTEGER 			REFERENCES users (user_id),
-    status 		CARD_STATUS_TYPE 	NOT NULL,
+    pack_id 		  INTEGER 			REFERENCES packs (pack_id),
+    user_id 		  INTEGER 			REFERENCES users (user_id),
+    status 		    CARD_STATUS_TYPE 	NOT NULL,
     last_visited 	DATE,
     to_be_learned_until DATE,
-    learned 		DATE
-
+    learned 		  DATE,
     PRIMARY KEY (pack_id, user_id)
 );
 
 CREATE TABLE user_cards (
-    user_id 		INTEGER 		REFERENCES users (user_id),
-    card_id 		INTEGER 		REFERENCES cards (card_id),
-    times_reviewed 	INTEGER 		NOT NULL,
-    correct_answers 	INTEGER 		NOT NULL CHECK (correct_answers <= times_reviewed)
-    status 		CARD_STATUS_TYPE NOT NULL
-
+    user_id 		      INTEGER 		REFERENCES users (user_id),
+    card_id 		      INTEGER 		REFERENCES cards (card_id),
+    times_reviewed 	  INTEGER 		NOT NULL,
+    correct_answers 	INTEGER 		NOT NULL CHECK (correct_answers <= times_reviewed),
+    status 		CARD_STATUS_TYPE NOT NULL,
     PRIMARY KEY (user_id, card_id)
 );
 
 CREATE TABLE group_packs (
     group_id 		INTEGER 			REFERENCES groups (group_id),
     pack_id 		INTEGER 			REFERENCES packs (pack_id),
-    status 		CARD_STATUS_TYPE 	NOT NULL
-
+    status 		CARD_STATUS_TYPE 	NOT NULL,
     PRIMARY KEY (group_id, pack_id)
 );
 
 CREATE TABLE rights ( -- editing (all packs) and access (private packs)
     group_id 	INTEGER 		REFERENCES groups (group_id),
     user_id	INTEGER 		REFERENCES users (user_id),
-    rights 	RIGHTS_TYPE 	NOT NULL
-    
+    rights 	RIGHTS_TYPE 	NOT NULL,
     PRIMARY KEY (group_id, user_id)
 );
 
 CREATE TABLE access_rights (
-    group_id 	INTEGER 	REFERENCES groups.group_id,
-    user_id 	INTEGER 	REFERENCES users.user_id
-    
+    group_id 	INTEGER 	REFERENCES groups (group_id),
+    user_id 	INTEGER 	REFERENCES users (user_id),
     PRIMARY KEY (group_id, user_id)
 );
