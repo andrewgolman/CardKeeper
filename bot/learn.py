55f7d483e41838bdee3a5e0fbbec297949cc50ce
andrewgolman <gas1998@mail.ru>
2017-05-04 22:11:11 +0300

    Create learn.py

---
+++ bot/learn.py
@@ -0,0 +1,21 @@
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+import queries
+import say
+from modes import LEARN
+
+def ask(bot, update):
+
+    return LEARN
+
+
+def show_all(bot, update):
+
+    return LEARN
+
+
+def shuffle(bot, update):
+
+    return LEARN
+
+
