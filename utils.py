c404c32c28b62f0070280ae2aa0f8a7aea696c66
Roman Nikonov <niconneo@yandex.ru>
2017-05-04 14:49:00 +0300

    Updated .gitignore. Added exception handler decorator (untested)

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
+import functools
+import logging
 
+
+def errors_ignore_and_log(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            logging.exception("Error")
+    return wrapped
