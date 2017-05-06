f846fcdc02030e7e9f09a6841a42e9475358e2ba
andrewgolman <gas1998@mail.ru>
2017-05-06 03:20:42 +0300

    Simplify functions with 'send'

--- bot/register.py
+++ bot/register.py
@@ -2,6 +2,7 @@ from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 import queries
 import say
+import utils
 
 GENERAL_GOAL, WEEKLY_GOAL, NOTIFY_LEARN, NOTIFY_STATS = (0, 1, 2, 3)
 
@@ -14,55 +15,59 @@ def start(bot, update):
     users_in_process[user_id] = [user_id, name]
 
     if queries.if_registered(user_id):
-        update.message.reply_text(say.hello)
+        utils.send(update, say.hello)
         menu(bot, update)
         return ConversationHandler.END
 
-    update.message.reply_text(say.welcome)
-
-    reply_keyboard = [['Speech', 'Belletristic', 'Science']]
-    update.message.reply_text(say.choose_general_goal,
-                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    utils.send(update, say.welcome)
+    utils.send(update, say.choose_general_goal, markup=say.GEN_GOAL_TYPE)
     return GENERAL_GOAL
 
 
 def general_goal(bot, update):
     general_goal = update.message.text
+    if general_goal not in say.GEN_GOAL_TYPE:
+        reply_keyboard = [say.GEN_GOAL_TYPE]
+        utils.send(update, say.incorrect_input + say.choose_general_goal, markup=say.GEN_GOAL_TYPE)
+        return GENERAL_GOAL
     users_in_process[update.message.from_user.id].append(general_goal)
-    update.message.reply_text(say.choose_weekly_goal)
+    utils.send(update, say.choose_weekly_goal)
     return WEEKLY_GOAL
 
 
 def weekly_goal(bot, update):
     weekly_goal = update.message.text
+    # можно ли скастовать к integer?
     users_in_process[update.message.from_user.id].append(weekly_goal)
-    reply_keyboard = [['Daily', 'Weekly', 'Never']]
-    update.message.reply_text(say.choose_learn_notifications,
-                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    utils.send(update, say.choose_learn_notifications, markup=say.NOTIFICATION_TYPE)
     return NOTIFY_LEARN
 
 
 def notify_learn(bot, update):
     notify_learn = update.message.text
+    if notify_learn not in say.NOTIFICATION_TYPE:
+        utils.send(update, say.incorrect_input + say.choose_learn_notifications, markup=say.NOTIFICATION_TYPE)
+        return NOTIFY_LEARN
     users_in_process[update.message.from_user.id].append(notify_learn)
-    reply_keyboard = [['Daily', 'Weekly', 'Never']]
-    update.message.reply_text(say.choose_stats_notifications,
-                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    utils.send(update, say.choose_stats_notifications, markup=say.NOTIFICATION_TYPE)
     return NOTIFY_STATS
 
 
 def notify_stats(bot, update):
     notify_stats = update.message.text
+    if notify_stats not in say.NOTIFICATION_TYPE:
+        utils.send(update, say.incorrect_input + say.choose_general_goal, markup=say.NOTIFICATION_TYPE)
+        return NOTIFY_STATS
     users_in_process[update.message.from_user.id].append(notify_stats)
-    update.message.reply_text(say.registration_completed)
+    utils.send(update, say.registration_completed)
     menu(bot, update)
-    print(users_in_process[update.message.from_user.id])
+    # print(users_in_process[update.message.from_user.id])
     queries.add_user(users_in_process.pop(update.message.from_user.id))
     return ConversationHandler.END
 
 
 def cancel(bot, update):
-    update.message.reply_text(say.register_to_access)
+    utils.send(update, say.register_to_access)
     return ConversationHandler.END
 
 
@@ -70,4 +75,4 @@ def menu(bot, update):
     ways = ["/Begin", "/Packs", "/Groups", "/Settings"]
     legend = say.menu_legend
     menu_opts = "\n".join(ways)
-    update.message.reply_text(legend + '\n' + menu_opts)
+    utils.send(update, legend + '\n' + menu_opts)
---
+++ bot/utils.py
@@ -0,0 +1,22 @@
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+
+import functools
+import logging
+
+
+def errors_ignore_and_log(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            logging.exception("Error")
+    return wrapped
+
+
+def send(update, text, markup=None):
+    if markup:
+        update.message.reply_text(text, reply_markup=ReplyKeyboardMarkup([markup], one_time_keyboard=True))
+    else:
+        update.message.reply_text(text)
