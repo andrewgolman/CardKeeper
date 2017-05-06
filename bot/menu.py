06382a4832bceb112077d2f1531065be5254824a
andrewgolman <gas1998@mail.ru>
2017-05-06 03:21:05 +0300

    Create menu.py

---
+++ bot/menu.py
@@ -0,0 +1,37 @@
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+
+
+def head_menu(bot, update):
+    ways = ["/Begin", "/Packs", "/Groups", "/Settings"]
+    legend = say.menu_legend
+    menu_opts = "\n".join(ways)
+    update.message.reply_text(legend + '\n' + menu_opts)
+
+
+def quit(bot, update):
+    pass
+
+
+def cancel(bot, update):
+    pass
+
+
+def end(bot, update):
+    pass
+
+
+def begin(bot, update):
+    pass
+
+
+def cards(bot, update):
+    pass
+
+
+def admin(bot, update):
+    pass
+
+
+def group_stats(bot, update):
+    pass
