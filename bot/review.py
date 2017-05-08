fff4f49249ed3ab5c3799eec0d578f9e284c01bc
andrewgolman <gas1998@mail.ru>
2017-05-08 13:48:26 +0300

    Add modes iteration handling

--- bot/review.py
+++ bot/review.py
@@ -1,30 +1,71 @@
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 import queries
-import utils
 import say
+import menu
+from utils import send, user
 
 CHOOSE_PACK, CHOOSE_REVIEW_TYPE, CHOOSE_LANGUAGE, ITERATE, END = tuple(range(5))
 
 review_types = ["Trust", "Enter"]
 review_state = {}
+languages = ["Front", "Back"]
+practise_markup = ["Proceed"]
+
+review_markup = ["Right", "Wrong"]
 
 
 class ReviewState:
     def __init__(self, items):
         self.cards = items
-    cards = []
-    wrong_answers = []
-    type = review_types[0]
-    language = 0
+        self.right_answers = []
+        self.wrong_answers = []
+        self.type = review_types[0]
+        self.language = 0
+        self.first_go = True
+        self.shuffle()
+
+    def move(self, user_id):
+        self.cards = self.wrong_answers
+        self.wrong_answers = []
+        if self.first_go:
+            for i in self.right_answers:
+                queries.update_card_data(user_id, i[0], 1)
+        for i in self.wrong_answers:
+            queries.update_card_data(user_id, i[0], 0)
+        self.first_go = False
+
+    # first card bug
+    def ask(self):
+        return self.cards[1][2 if self.language else 1]
+
+    def answer(self):
+        return self.cards[1][1 if self.language else 2]
+
+    def right(self):
+        pass
+
+    def wrong(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def next(self):
+        return self.cards[1]
+
+    def check(self):
+        if True:
+            return None
+        return self.cards[0][2 if self.language else 1]
 
 
 def choose_pack(bot, update):
     packs = queries.active_packs(update.message.from_user.id)
     if not packs:
-        utils.send(update, say.no_packs_available)
+        send(update, say.no_packs_available)
         return ConversationHandler.END
-    utils.send(update, say.enumerated(packs))
+    send(update, say.enumerated(packs))
     return CHOOSE_PACK
 
 
@@ -32,65 +73,82 @@ def pack_chosen(bot, update):
     try:
         pack_id = queries.active_packs(update.message.from_user.id)[int(update.message.text) - 1][0]
     except TypeError or IndexError:
-        utils.send(update, say.incorrect_input)
+        send(update, say.incorrect_input)
         return choose_pack(bot, update)
-    cards = queries.select_cards(utils.user(update), pack_id)
+    cards = queries.select_cards(user(update), pack_id)
     if not cards:
-        utils.send(update, say.pack_is_empty)
+        send(update, say.pack_is_empty)
         return choose_pack(bot, update)
-    review_state[utils.user(update)] = ReviewState(cards)
+    review_state[user(update)] = ReviewState(cards)
     return choose_review_type(bot, update)
 
 
 def choose_review_type(bot, update):
-    utils.send(update, say.choose_type_of_review, markup=[review_types])
+    send(update, say.choose_type_of_review, markup=[review_types])
     return CHOOSE_REVIEW_TYPE
 
 
 def review_type_chosen(bot, update):
     if update.message.text not in review_types:
-        utils.send(update, say.incorrect_input, markup=[review_types])
+        send(update, say.incorrect_input, markup=[review_types])
         return choose_review_type(bot, update)
-    review_state[utils.user(update)].type = update.message.text
+    review_state[user(update)].type = update.message.text
     return choose_language(bot, update)
 
 
 def choose_language(bot, update):
-    utils.send(update, say.choose_language(review_state[utils.user(update)].cards[0]))
+    send(update, say.choose_language(review_state[user(update)].cards[0]), markup=languages)
     return CHOOSE_LANGUAGE
 
 
 def language_chosen(bot, update):
-    if False:
-        utils.send(update, say.incorrect_input)
+    if update.message.text not in languages:
+        send(update, say.incorrect_input)
         return choose_language(bot, update)
-    review_state[utils.user(update)].language = 0
+    review_state[user(update)].language = (0 if update.message.text == languages[0] else 1)
     return ITERATE
 
 
 def review_ask(bot, update):
+    send(update, review_state[user(update)].next()[2 if review_state[user(update)].language else 1], markup=[review_markup])
     return ITERATE
 
 
-def review_check():
-    return review_ask
+def review_check(bot, update):
+    send(update, review_state[user(update)].next()[1 if review_state[user(update)].language else 2])
+    if update.message.text == review_markup[0]:
+        review_state[user(update)].right()
+    else:
+        review_state[user(update)].wrong()
+    return review_ask(bot, update)
 
 
-def review_end():
-    return ConversationHandler.END
-
-
-def test_ask():
+def test_ask(bot, update):
+    test_markup = review_state[user(update)].test_markup()
+    send(update, review_state[user(update)].next()[2 if review_state[user(update)].language else 1], markup=[test_markup])
     return ITERATE
 
 
-def test_check():
-    return review_ask
+def test_check(bot, update):
+    true_ans = review_state[user(update)].compare(update.message.text)
+    if not true_ans:
+        send(update, say.right)
+    else:
+        send(update, say.wrong(true_ans))
+    return test_ask(bot, update)
 
 
-def practise_ask():
-    pass
+def practise_ask(bot, update):
+    send(update, review_state[user(update)].next()[2 if review_state[user(update)].language else 1], markup=[practise_markup])
+    return ITERATE
 
 
-def practise_end():
-    pass
+def end(bot, update):
+    if not review_state[user(update)].wrong_answers:
+        send(update, say.completed)
+        menu.head_menu(bot, update)
+        return ConversationHandler.END
+    send(update, say.inter_results(review_state[user(update)]))
+    review_state[user(update)].move(user(update))
+    return ITERATE
+
