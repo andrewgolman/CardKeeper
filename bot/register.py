2bfbf139e75871c381f67fc79344c14298badfd8
andrewgolman <gas1998@mail.ru>
2017-05-11 23:11:25 +0300

    Incorrect input situations are handled correctly now

--- bot/register.py
+++ bot/register.py
@@ -2,7 +2,8 @@ from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from db import queries
 import say
-import utils
+import menu
+from utils import send, user
 
 GENERAL_GOAL, WEEKLY_GOAL, NOTIFY_LEARN, NOTIFY_STATS = (0, 1, 2, 3)
 
@@ -10,69 +11,82 @@ users_in_process = {}
 
 
 def start(bot, update):
-    user_id = update.message.from_user.id
+    user_id = user(update)
     name = update.message.from_user.username
     users_in_process[user_id] = [user_id, name]
 
     if queries.if_registered(user_id):
-        utils.send(update, say.hello)
-        menu(bot, update)
+        send(update, say.hello)
+        menu.head_menu(bot, update)
         return ConversationHandler.END
 
-    utils.send(update, say.welcome)
-    utils.send(update, say.choose_general_goal, markup=say.GEN_GOAL_TYPE)
+    send(update, say.welcome)
+    return general_goal(update)
+
+
+def general_goal(update):
+    send(update, say.choose_general_goal, markup=say.GEN_GOAL_TYPE)
     return GENERAL_GOAL
 
 
-def general_goal(bot, update):
-    general_goal = update.message.text
-    if general_goal not in say.GEN_GOAL_TYPE:
+def general_goal_handle(bot, update):
+    res = update.message.text
+    if res not in say.GEN_GOAL_TYPE:
         reply_keyboard = [say.GEN_GOAL_TYPE]
-        utils.send(update, say.incorrect_input + say.choose_general_goal, markup=say.GEN_GOAL_TYPE)
+        send(update, say.incorrect_input + say.choose_general_goal, markup=say.GEN_GOAL_TYPE)
         return GENERAL_GOAL
-    users_in_process[update.message.from_user.id].append(general_goal)
-    utils.send(update, say.choose_weekly_goal)
+    users_in_process[user(update)].append(res)
+    return weekly_goal(update)
+
+
+def weekly_goal(update):
+    send(update, say.choose_weekly_goal)
     return WEEKLY_GOAL
 
 
-def weekly_goal(bot, update):
-    weekly_goal = update.message.text
-    # можно ли скастовать к integer?
-    users_in_process[update.message.from_user.id].append(weekly_goal)
-    utils.send(update, say.choose_learn_notifications, markup=say.NOTIFICATION_TYPE)
+def weekly_goal_handle(bot, update):
+    try:
+        res = int(update.message.text)
+        if not 1000 > res > 0:
+            raise TypeError
+    except TypeError:
+        send(update, say.incorrect_weekly_goal)
+        return WEEKLY_GOAL
+    users_in_process[user(update)].append(res)
+    return notify_learn(update)
+
+
+def notify_learn(update):
+    send(update, say.choose_learn_notifications, markup=say.NOTIFICATION_TYPE)
     return NOTIFY_LEARN
 
 
-def notify_learn(bot, update):
-    notify_learn = update.message.text
-    if notify_learn not in say.NOTIFICATION_TYPE:
-        utils.send(update, say.incorrect_input + say.choose_learn_notifications, markup=say.NOTIFICATION_TYPE)
+def notify_learn_handle(bot, update):
+    res = update.message.text
+    if res not in say.NOTIFICATION_TYPE:
+        send(update, say.incorrect_input + say.choose_learn_notifications, markup=say.NOTIFICATION_TYPE)
         return NOTIFY_LEARN
-    users_in_process[update.message.from_user.id].append(notify_learn)
-    utils.send(update, say.choose_stats_notifications, markup=say.NOTIFICATION_TYPE)
+    users_in_process[user(update)].append(res)
+    return notify_stats(update)
+
+
+def notify_stats(update):
+    send(update, say.choose_stats_notifications, markup=say.NOTIFICATION_TYPE)
     return NOTIFY_STATS
 
 
-def notify_stats(bot, update):
-    notify_stats = update.message.text
-    if notify_stats not in say.NOTIFICATION_TYPE:
-        utils.send(update, say.incorrect_input + say.choose_general_goal, markup=say.NOTIFICATION_TYPE)
+def notify_stats_handle(bot, update):
+    res = update.message.text
+    if res not in say.NOTIFICATION_TYPE:
+        send(update, say.incorrect_input + say.choose_general_goal, markup=say.NOTIFICATION_TYPE)
         return NOTIFY_STATS
-    users_in_process[update.message.from_user.id].append(notify_stats)
-    utils.send(update, say.registration_completed)
-    menu(bot, update)
-    # print(users_in_process[update.message.from_user.id])
-    queries.add_user(users_in_process.pop(update.message.from_user.id))
+    users_in_process[user(update)].append(res)
+    send(update, say.registration_completed)
+    menu.head_menu(bot, update)
+    queries.add_user(users_in_process.pop(user(update)))
     return ConversationHandler.END
 
 #
 # def cancel(bot, update):
-#     utils.send(update, say.register_to_access)
+#     send(update, say.register_to_access)
 #     return ConversationHandler.END
-
-
-def menu(bot, update):
-    ways = ["/begin", "/packs", "/cards", "/groups", "/settings"]
-    legend = say.menu_legend
-    menu_opts = "\n".join(ways)
-    utils.send(update, legend + '\n' + menu_opts)
