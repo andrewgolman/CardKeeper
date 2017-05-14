36b8c93e5b900cda1fa68a39f1ce1b3d28160507
andrewgolman <gas1998@mail.ru>
2017-05-14 19:52:59 +0300

    Move mode wrapping to modes.py

--- bot/utils.py
+++ bot/utils.py
@@ -42,26 +42,3 @@ def send(update, text, markup=None, one_time_keyboard=True):
 
 def user(update):
     return update.message.from_user.id
-
-
-inf_const = 2
-CHOOSE_PACK, QUIT = tuple(range(2))
-
-
-def choose_pack(bot, update):
-    packs = queries.active_packs(user(update))
-    if not packs:
-        send(update, say.no_packs_available)
-        return QUIT
-    send(update, say.enumerated(packs))
-    return CHOOSE_PACK
-
-
-def get_pack_id(update):
-    try:
-        pack_id = queries.active_packs(update.message.from_user.id)[int(update.message.text) - 1][0]
-    except (TypeError, IndexError, ValueError):
-        send(update, say.incorrect_input)
-        return None
-    return pack_id
-
